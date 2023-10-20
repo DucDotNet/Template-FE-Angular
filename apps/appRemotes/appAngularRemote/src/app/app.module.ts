d4fbee3fbce3599ea8a0aa1118bf814de1948b67
DucDotNet <dev.ducdv@gmail.com>
2023-10-20 09:35:30 +0700

    AppRemote: build app angular

---
+++ apps/appRemotes/appAngularRemote/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {
+  ButtonModule,
+  DatetimePickerModule,
+  AvatarModule,
+} from '@hiro/base-component-ant/src/public-api';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DatetimePickerModule,
+    AvatarModule,
+    ButtonModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
